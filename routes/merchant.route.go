d879a3e56a828f28900aa923bcdbdb634d64c75d
Adib Setiawan <itsmeadib.s@gmail.com>
2022-12-29 16:45:24 +0700

    add session and handling logout

--- routes/merchant.route.go
+++ routes/merchant.route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/adibSetiawann/transaction-api-go/controller"
+	"github.com/adibSetiawann/transaction-api-go/middleware"
 	repository "github.com/adibSetiawann/transaction-api-go/repository/merchant"
 	service "github.com/adibSetiawann/transaction-api-go/service/merchant"
 	"github.com/gofiber/fiber/v2"
@@ -11,12 +12,10 @@ func MerchantRoute(app *fiber.App) {
 	merchantRepo := repository.NewMerchantRepository()
 	merchantService := service.NewMerchantService(&merchantRepo)
 	merchantController := controller.NewMerchantController(&merchantService)
+	
 	app.Get("/merchants", merchantController.GetAll)
 	app.Get("/merchants/:id", merchantController.GetById)
-	// app.Post("/merchants", middleware.AuthAsAdmin, merchantController.Create)
-	// app.Put("/merchants/update/:id", middleware.AuthAsAdmin, merchantController.Update)
-	// app.Delete("/merchants/delete/:id", middleware.AuthAsAdmin, merchantController.Remove)
-	app.Post("/merchants", merchantController.Create)
-	app.Put("/merchants/update/:id", merchantController.Update)
-	app.Delete("/merchants/delete/:id", merchantController.Remove)
+	app.Post("/merchants", middleware.AuthAsAdmin, merchantController.Create)
+	app.Put("/merchants/update/:id", middleware.AuthAsAdmin, merchantController.Update)
+	app.Delete("/merchants/delete/:id", middleware.AuthAsAdmin, merchantController.Remove)
 }
